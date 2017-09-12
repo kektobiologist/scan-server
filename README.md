dd7036ca7d630e05756a5fab712d247d3731d8cf
arpit <arpit.tarang@gmail.com>
2017-09-12 13:15:38 +0530

    added readme. added /scan

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # scan-server
 network scanner server
+
+## Instructions
+
+* `sudo apt-get install sane imagemagick`
+* edit `/etc/sane.d/dll.conf` to have only `hp` uncommented
+* setup daemon to host server
---
+++ index.js
@@ -0,0 +1,37 @@
+const express = require('express')
+const app = express()
+
+const exec = require('child_process').exec;
+
+const cmd = "scanimage --format=tiff >/tmp/image.tiff && convert -quality 100 /tmp/image.tiff /tmp/image.jpg"
+
+var RateLimit = require('express-rate-limit');
+var limiter = new RateLimit({
+  windowMs: 5*1000, // 5 seconds 
+  max: 1, // limit each IP to 1 requests per windowMs 
+  delayMs: 0 // disable delaying - full speed until the max limit is reached 
+});
+ 
+//  apply to all requests 
+app.use(limiter);
+
+app.get('/', function (req, res) {
+  res.send('Hello World!')
+})
+
+app.get('/scan', (req, res) => {
+  exec(cmd, (err, stdout, stderr) => {
+    if (err) {
+      console.log(err)
+      res.status(400)
+      res.send('error in scan')
+    } else {
+      console.log('got response: ' + stdout + stderr)
+      res.sendFile('/tmp/image.jpg')
+    }
+  })
+})
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!')
+})
