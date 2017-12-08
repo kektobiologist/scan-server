d869f788edc2e62dc0f9f92f670088aff1eb5cb8
arpit <arpit.tarang@gmail.com>
2017-12-08 12:56:20 +0530

    added https with own cert

--- index.js
+++ index.js
@@ -1,62 +1,74 @@
-const express = require('express')
-var cors = require('cors')
-const app = express()
+const express = require("express");
+var cors = require("cors");
+const app = express();
+var fs = require("fs");
+var http = require("http");
+var https = require("https");
+var privateKey = fs.readFileSync("key.pem", "utf8");
+var certificate = fs.readFileSync("cert.pem", "utf8");
+var credentials = { key: privateKey, cert: certificate };
 
-app.use(cors())
+app.use(cors());
 
-const exec = require('child_process').exec;
+const exec = require("child_process").exec;
 
-const cmd = "scanimage --compression None --resolution 100 --format=tiff >/tmp/image.tiff && convert -quality 100 /tmp/image.tiff /tmp/image.jpg"
+const cmd =
+  "scanimage --compression None --resolution 100 --format=tiff >/tmp/image.tiff && convert -quality 100 /tmp/image.tiff /tmp/image.jpg";
 
-var RateLimit = require('express-rate-limit');
+var RateLimit = require("express-rate-limit");
 var limiter = new RateLimit({
-  windowMs: 5*1000, // 5 seconds 
-  max: 1, // limit each IP to 1 requests per windowMs 
-  delayMs: 0 // disable delaying - full speed until the max limit is reached 
+  windowMs: 5 * 1000, // 5 seconds
+  max: 1, // limit each IP to 1 requests per windowMs
+  delayMs: 0 // disable delaying - full speed until the max limit is reached
 });
- 
-//  apply to all requests 
+
+//  apply to all requests
 app.use(limiter);
 
-var fs = require('fs');
+var fs = require("fs");
 
 // function to encode file data to base64 encoded string
 function base64_encode(file) {
-    // read binary data
-    var bitmap = fs.readFileSync(file);
-    // convert binary data to base64 encoded string
-    return new Buffer(bitmap).toString('base64');
+  // read binary data
+  var bitmap = fs.readFileSync(file);
+  // convert binary data to base64 encoded string
+  return new Buffer(bitmap).toString("base64");
 }
 
-app.get('/', function (req, res) {
-  res.send('Hello World!')
-})
+app.get("/", function(req, res) {
+  res.send("Hello World!");
+});
 
-app.get('/scan/test', (req, res) => {
+app.get("/scan/test", (req, res) => {
   // serve local image
-  var base64str = base64_encode('image.jpg');
-  res.send(base64str)
-})
+  var base64str = base64_encode("image.jpg");
+  res.send(base64str);
+});
 
-app.get('/scan/testImage', (req, res) => {
-  res.sendFile('image.jpg')
-})
+app.get("/scan/testImage", (req, res) => {
+  res.sendFile("image.jpg");
+});
 
-
-app.get('/scan', (req, res) => {
+app.get("/scan", (req, res) => {
   exec(cmd, (err, stdout, stderr) => {
     if (err) {
-      console.log(err)
-      res.status(400)
-      res.send(err)
+      console.log(err);
+      res.status(400);
+      res.send(err);
     } else {
-      var base64str = base64_encode('/tmp/image.jpg');
-      res.send(base64str)
+      var base64str = base64_encode("/tmp/image.jpg");
+      res.send(base64str);
     }
-  })
-})
+  });
+});
 
-port = 8899
-app.listen(port, function () {
-  console.log('Example app listening on port ' + port)
-})
+var httpServer = http.createServer(app);
+var httpsServer = https.createServer(credentials, app);
+httpPort = 8899;
+httpsPort = 8900;
+
+httpServer.listen(httpPort);
+httpsServer.listen(httpsPort);
+// app.listen(port, function () {
+//   console.log('Example app listening on port ' + port)
+// })
